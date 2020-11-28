a0c7d55c853b875c2a397e7e16a9104217161589
isfahany <hanyisfa@gmail.com>
2020-11-28 10:46:48 +0700

    benerin file upload

--- file_upload/file_upload/uploader.php
+++ file_upload/file_upload/uploader.php
@@ -21,23 +21,13 @@ $target_dir = "upload/";
 $target_file = $target_dir . (basename($_FILES["fileToUpload"]["name"]));
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$whitelist_type = array('image/jpeg', 'image/png');
+#$whitelist_type = array('image/jpeg', 'image/png');
 
 if(isset($_POST["submit"])){
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check == true){
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
-    }
-}
-
-if(isset($_GET['img'])){
-    $image = $_GET['img'];
-    $imageExt = strtolower(pathinfo($image,PATHINFO_EXTENSION));
-    if($imageExt != "jpg" && $imageExt != "png"  && $imageExt != "jpeg"){
-        echo "sorry, Only JPG, JPEG, PNG files are allowed.";
-    }else{
-        echo '<img src= "'.$image.'">';
     }
 }
 
@@ -51,11 +41,11 @@ if ($_FILES["fileToUpload"]["size"] > 6000000) {
     $uploadOk = 0;
 }
 
-if (!in_array($_FILES['fileToUpload']['type'], $whitelist_type)){
-    #echo "<h1>HERE</h1>";
-    echo $_FILES['fileToUpload']['type'];
-    $uploadOk = 0;
-}
+#if (!in_array($_FILES['fileToUpload']['type'], $whitelist_type)){
+#    #echo "<h1>HERE</h1>";
+#    echo $_FILES['fileToUpload']['type'];
+#    $uploadOk = 0;
+#}
 
 
 if ($uploadOk == 0) {
